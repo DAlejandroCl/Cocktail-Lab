65e7a7411bd0882c0358a8addfc8099329a45c8e
Diego <alejandro1clavijo3@gmail.com>
2026-02-02 18:53:42 -0500

    feat: setup Zustand store with Slice Pattern
    - Create recipe slice with fetch, search, and select actions
    - Add modal state management
    - Configure devtools middleware

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,39 +1,48 @@
-import type { StateCreator } from "zustand";
-import { getCategories, getRecipes } from "../services/RecipeService";
-import type { Category, SearchFilters, Drink } from "../types";
+import type { StateCreator } from "zustand"
+import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService"
+import type { Category, Drink, Drinks, RecipeDetail, SearchFilters } from "../types"
 
 export type RecipesSliceType = {
-  categories: Category[];
-  recipes: Drink[];
-  fetchCategories: () => Promise<void>;
-  searchRecipes: (filters: SearchFilters) => Promise<void>;
-};
+    categories: Category[]
+    drinks: Drinks
+    selectedRecipe: RecipeDetail
+    modal: boolean
+    fetchCategories: () => Promise<void>
+    searchRecipes: (searchFilters: SearchFilters) => Promise<void>
+    selectRecipe: (id: Drink['idDrink']) => Promise<void>
+    closeModal: () => void
+}
 
-export const createRecipesSlice: StateCreator<
-  RecipesSliceType,
-  [["zustand/devtools", never]],
-  []
-> = (set) => ({
-  categories: [],
-  recipes: [],
-
-  fetchCategories: async () => {
-    try {
-      const categories = await getCategories();
-      set({ categories });
-    } catch (error) {
-      console.error("Error fetching categories:", error);
+export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
+    categories: [],
+    drinks: {
+        drinks: []
+    },
+    selectedRecipe: {} as RecipeDetail,
+    modal: false,
+    fetchCategories: async () => {
+        const categories = await getCategories()
+        set({
+            categories: categories
+        })
+    },
+    searchRecipes: async (filters) => {
+       const drinks = await getRecipes(filters)
+       set({
+            drinks: { drinks }
+       })
+    },
+    selectRecipe: async (id) => {
+        const selectedRecipe = await getRecipeById(id)
+        set({
+            selectedRecipe,
+            modal: true
+        })
+    },
+    closeModal: () => {
+        set({
+            modal: false,
+            selectedRecipe: {} as RecipeDetail
+        })
     }
-  },
-
-  searchRecipes: async (filters) => {
-    try {
-      const recipes = await getRecipes(filters);
-      set({ recipes });
-      console.log("Recipes found:", recipes);
-    } catch (error) {
-      console.error("Error searching recipes:", error);
-      set({ recipes: [] });
-    }
-  },
-});
+})
