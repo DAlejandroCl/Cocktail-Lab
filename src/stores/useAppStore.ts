90f5896d5dca3f7f3e91aa2caa506f00998de69e
Diego <alejandro1clavijo3@gmail.com>
2026-02-03 21:55:54 -0500

    feat(store): integrate favorites slice into global store

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { createRecipesSlice, type RecipesSliceType } from "./recipeSlice";
+import { createFavoritesSlice, type FavoritesSliceType } from "./favoritesSlice";
 
-export const useAppStore = create<RecipesSliceType>()(
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(
   devtools((...args) => ({
     ...createRecipesSlice(...args),
+    ...createFavoritesSlice(...args)
   }))
 );
