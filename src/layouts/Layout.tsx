2ee63b4ce89a97a4e52cea1767aa6ff52326c34c
Diego <alejandro1clavijo3@gmail.com>
2026-02-03 21:55:05 -0500

    chore(layout): align layout structure with updated header

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,16 +1,13 @@
-import { Outlet } from "react-router-dom"
-import Header from "../components/Header"
-import Modal from "../components/Modal"
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Modal from "../components/Modal";
 
 export default function Layout() {
   return (
-    <>
+    <div className="min-h-screen">
       <Header />
-      
-      <main className="container mx-auto py-16">
-        <Outlet />
-      </main>
+      <Outlet />
       <Modal />
-    </>
-  )
+    </div>
+  );
 }
