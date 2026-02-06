dd4698e58370984e2191d7b3d32475b60d99e89d
Diego <alejandro1clavijo3@gmail.com>
2026-02-05 22:35:04 -0500

    refactor(store): enhance recipe search flow and global store composition

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -3,46 +3,53 @@ import { getCategories, getRecipeById, getRecipes } from "../services/RecipeServ
 import type { Category, Drink, Drinks, RecipeDetail, SearchFilters } from "../types"
 
 export type RecipesSliceType = {
-    categories: Category[]
-    drinks: Drinks
-    selectedRecipe: RecipeDetail
-    modal: boolean
-    fetchCategories: () => Promise<void>
-    searchRecipes: (searchFilters: SearchFilters) => Promise<void>
-    selectRecipe: (id: Drink['idDrink']) => Promise<void>
-    closeModal: () => void
-}
+  categories: Category[];
+  drinks: Drinks;
+  selectedRecipe: RecipeDetail;
+  modal: boolean;
+  isLoading: boolean;
+  hasSearched: boolean;
+  fetchCategories: () => Promise<void>;
+  searchRecipes: (searchFilters: SearchFilters) => Promise<void>;
+  selectRecipe: (id: Drink["idDrink"]) => Promise<void>;
+  closeModal: () => void;
+};
 
-export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
-    categories: [],
-    drinks: {
-        drinks: []
-    },
-    selectedRecipe: {} as RecipeDetail,
-    modal: false,
-    fetchCategories: async () => {
-        const categories = await getCategories()
-        set({
-            categories: categories
-        })
-    },
-    searchRecipes: async (filters) => {
-       const drinks = await getRecipes(filters)
-       set({
-            drinks: { drinks }
-       })
-    },
-    selectRecipe: async (id) => {
-        const selectedRecipe = await getRecipeById(id)
-        set({
-            selectedRecipe,
-            modal: true
-        })
-    },
-    closeModal: () => {
-        set({
-            modal: false,
-            selectedRecipe: {} as RecipeDetail
-        })
-    }
-})
+
+export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
+  categories: [],
+  drinks: { drinks: [] },
+  selectedRecipe: {} as RecipeDetail,
+  modal: false,
+  isLoading: false,
+  hasSearched: false,
+
+  fetchCategories: async () => {
+    const categories = await getCategories();
+    set({ categories });
+  },
+
+  searchRecipes: async (filters) => {
+  set({ isLoading: true, hasSearched: true });
+
+  try {
+    const drinks = await getRecipes(filters);
+    set({ drinks: { drinks } });
+  } finally {
+    set({ isLoading: false });
+  }
+},
+
+
+  selectRecipe: async (id) => {
+    const selectedRecipe = await getRecipeById(id);
+    set({ selectedRecipe, modal: true });
+  },
+
+  closeModal: () => {
+    set({
+      modal: false,
+      selectedRecipe: {} as RecipeDetail,
+    });
+  },
+});
--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -4,8 +4,8 @@ import { createRecipesSlice, type RecipesSliceType } from "./recipeSlice";
 import { createFavoritesSlice, type FavoritesSliceType } from "./favoritesSlice";
 
 export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(
-  devtools((...args) => ({
-    ...createRecipesSlice(...args),
-    ...createFavoritesSlice(...args)
+  devtools((...a) => ({
+    ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a),
   }))
 );
