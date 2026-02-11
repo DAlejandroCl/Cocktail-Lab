2ab763aa47f769ef4a64a0aad7ed4aed67b2c17f
Diego <alejandro1clavijo3@gmail.com>
2026-02-11 17:33:47 -0500

    refactor(router): implement nested routing architecture with route-based code splitting

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
 import Header from "../components/Header";
 import Modal from "../components/Modal";
 import Notification from "../components/Notification";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <Outlet />
+      <Suspense fallback={null}>
+        <main className="flex-1">
+          <Outlet />
+        </main>
+      </Suspense>
       <Modal />
       <Notification />
     </div>
--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import IndexPage from './views/IndexPage'
-import FavoritesPage from './views/FavoritesPage'
+import { lazy } from "react";
 import Layout from "./layouts/Layout";
+
+const IndexPage = lazy(() => import("./views/IndexPage"));
+const FavoritesPage = lazy(() => import("./views/FavoritesPage"));
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<IndexPage />} index/>
-          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route index element={<IndexPage />} />
+          <Route path="favorites" element={<FavoritesPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
